91aed80c74ea4ac634bdfb1d5d031772491cf4f6
Izabela K <izagaj1990@gmail.com>
2017-11-11 17:36:03 +0100

    First exercise.

---
+++ src/main/java/FirstExercise.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FirstExercise {
+
+    public static void main (String[] args){
+
+
+        String text = "Witaj świecie";
+        System.out.println(text);
+        Scanner scanner = new Scanner(System.in);
+        String answer = null;
+        do {
+            System.out.println("Aby zamknąć aplikację wpisz 'exit' i wciśnij enter.");
+            answer = scanner.nextLine();
+        } while(!answer.equals("exit"));
+    }
+}
